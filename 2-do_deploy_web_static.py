88d6b2b4952bb12bbeab1fe1009215be1f5a9e66
Nachop51 <nachoperalta0@gmail.com>
2022-08-25 21:28:04 +0000

    checking after command

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -12,17 +12,28 @@ def do_deploy(archive_path):
         return False
     filename = archive_path.split('/')[1][:-4]
     o1 = put(archive_path, "/tmp/{}.tgz".format(filename))
+    if o1.failed:
+        return False
     o2 = run("mkdir -p /data/web_static/releases/{}/".format(filename))
+    if o2.failed:
+        return False
     o3 = run("tar -zxf /tmp/{}.tgz -C /data/web_static/releases/{}/"
              .format(filename, filename))
+    if o3.failed:
+        return False
     o4 = run("rm /tmp/{}.tgz".format(filename))
+    if o4.failed:
+        return False
     o5 = run("rm -fr /data/web_static/current")
+    if o5.failed:
+        return False
     o6 = run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
              .format(filename))
+    if o6.failed:
+        return False
     o7 = run("mv /data/web_static/releases/{}"
              "/web_static/* /data/web_static/releases/{}/"
              .format(filename, filename))
-    for o in [o1, o2, o3, o4, o5, o6, o7]:
-        if o.failed:
-            return False
+    if o7.failed:
+        return False
     return True
