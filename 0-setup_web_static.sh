f3e94cfac9442f1ef80de89b8a6f90e12f81f406
Nachop51 <nachoperalta0@gmail.com>
2022-08-25 14:05:31 +0000

    changing html

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -8,7 +8,13 @@ mkdir -p /data/web_static/
 mkdir -p /data/web_static/releases/
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
-echo "<html><body>Hoooooolberton</body></html>" > /data/web_static/releases/test/index.html
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test /data/web_static/current
 chown -R ubuntu:ubuntu /data/
 sed -i "/server_name _/a location \/hbnb_static\/ {\n\talias \/data\/web_static\/current\/;\n}" /etc/nginx/sites-available/default
